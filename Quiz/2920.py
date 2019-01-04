a16784414ea54a8e7cd094f9ce20ae54e5c71c46
Mijin <mijin5122@naver.com>
2019-01-04 17:44:03 +0900

    D03 | 190104 PM | Recursive Function

--- Quiz/2920.py
+++ Quiz/2920.py
@@ -1,9 +1,22 @@
 num=[]
 scale = map(int,input().split())
-
+cnt = 0
 for i in scale:
     num.append(i)
 
-print(len(num))
-# for i in range(len(num)):
-#     if i
+for i in range(1,len(num)):
+    if abs(num[i-1] - num[i])==1:
+        if num[i-1]>num[i]:
+            cnt = 0
+        else:
+            cnt = 1
+    else:
+        cnt = 2
+        break
+
+if cnt==0:
+    print("descending")
+elif cnt==1:
+    print("ascending")
+else:
+    print("mixed")        
